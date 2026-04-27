607f9a45333b00c772f71d3d7fdd5184b93128ec
Joseph Uchiha <jowzefy@gmail.com>
2026-04-27 11:10:08 +0330

    Create constants.py

---
+++ SpyfallApp/client/screens/utils/constants.py
@@ -0,0 +1,9 @@
+SERVER_URL = "ws://localhost:8765"
+LOCATIONS = [
+    "Airplane", "Bank", "Beach", "Circus Tent", "Corporate Party",
+    "Crusader Army", "Day Spa", "Embassy", "Hospital", "Hotel",
+    "Military Base", "Movie Studio", "Ocean Liner", "Passenger Train",
+    "Pirate Ship", "Polar Station", "Police Station", "Restaurant",
+    "School", "Service Station", "Space Station", "Submarine",
+    "Supermarket", "Theater", "University", "Wedding", "Zoo"
+]
